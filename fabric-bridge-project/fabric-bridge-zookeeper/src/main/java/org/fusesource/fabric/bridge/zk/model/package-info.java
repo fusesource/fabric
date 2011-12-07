9735b9350282df6e206b276184ca5e975413062e
James Strachan <james.strachan@gmail.com>
2011-12-07 09:58:51 +0000

    code now moved to https://github.com/fusesource/fuse

--- fabric-bridge-project/fabric-bridge-zookeeper/src/main/java/org/fusesource/fabric/bridge/zk/model/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace="http://fusesource.org/fabric/bridge/zookeeper", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.fusesource.fabric.bridge.zk.model;
--- fabric-bridge-project/fabric-bridge/src/main/java/org/fusesource/fabric/bridge/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace="http://fusesource.org/fabric/bridge", elementFormDefault=javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.fusesource.fabric.bridge;
